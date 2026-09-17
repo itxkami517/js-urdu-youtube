89a592d3d4255fc1b536799d03888cb170a7f323
Kamran khan <kamranorkz517@gmail.com>
2026-09-17 19:22:41 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+console.log("My name is Mr Kami");
+
+let Name = "mr kami";
+let Age = 20;
+const Country = "pakistan";
+
+Name = "kamran khan";
+
+console.table([Name, Age, Country]);
+
+
+// practice 
+console.log(20 + 10);
+console.log(20 - 10);
+console.log(20 * 10);
+console.log(20 / 10);
+console.log("20" + "10");
+
+
+let studentName = "Kami";
+let age = 20;
+let marks = 85;
+
+console.log(studentName);
+console.log(age);
+console.log(marks);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const name = "Kami";
+const age = 20;
+const isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+console.log(typeof name);
+console.log(typeof "kami");
