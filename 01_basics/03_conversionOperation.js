acce5241272634f36755ac3c005e12f08c4ff46b
Kamran khan <kamranorkz517@gmail.com>
2026-09-24 18:26:00 +0000

    ConversionOfDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let value = "kami";
+
+console.log(value);
+console.log(typeof value);
+
+let ValueInNumber = Number(value);
+
+console.log(ValueInNumber);
+console.log(typeof ValueInNumber);
+
+let number = 1;
+
+let ValueIBoolean = Boolean(number);
+
+console.log(ValueIBoolean);
+console.log(typeof ValueIBoolean);
+
+let Extra = " ";
+
+let ExtraIBoolean = Boolean(Extra);
+
+console.log(ExtraIBoolean);
+console.log(typeof ExtraIBoolean);
+
